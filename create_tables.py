9a4e29a57282f30d2f80e6e666162da986a46fe1
danielkiselev <32784081+danielkiselev@users.noreply.github.com>
2025-06-27 19:21:31 -0400

    updated create table

--- create_tables.py
+++ create_tables.py
@@ -4,47 +4,22 @@ from config import load_config
 def create_tables():
     """ Create tables in the PostgreSQL database"""
     commands = (
-        """
-        CREATE TABLE lineitems (
+        """CREATE TABLE lineitems (
             lineitem_id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             price VARCHAR(255) NOT NULL,
             qty int NOT NULL,
             ID_INVOICE VARCHAR(255) NOT NULL
-        )
+        )""",
             
-        CREATE TABLE INVOICE (
+        """CREATE TABLE INVOICE (
             INVOICEID SERIAL PRIMARY KEY,
             PAYMENTSTATUS boolean,
             CLIENTID VARCHAR(255) NOT NULL,
             DESCRIPTION VARCHAR(255) NOT NULL,
-            RECIPIENTEMAIL VARCHAR(255) NOT NULL,
-        )
-
-        """,
-        """
-        # CREATE TABLE part_drawings (
-        #         part_id INTEGER PRIMARY KEY,
-        #         file_extension VARCHAR(5) NOT NULL,
-        #         drawing_data BYTEA NOT NULL,
-        #         FOREIGN KEY (part_id)
-        #         REFERENCES parts (part_id)
-        #         ON UPDATE CASCADE ON DELETE CASCADE
-        # )
-        """,
-        """
-        # CREATE TABLE vendor_parts (
-        #         vendor_id INTEGER NOT NULL,
-        #         part_id INTEGER NOT NULL,
-        #         PRIMARY KEY (vendor_id , part_id),
-        #         FOREIGN KEY (vendor_id)
-        #             REFERENCES vendors (vendor_id)
-        #             ON UPDATE CASCADE ON DELETE CASCADE,
-        #         FOREIGN KEY (part_id)
-        #             REFERENCES parts (part_id)
-        #             ON UPDATE CASCADE ON DELETE CASCADE
-        # )
-        # """)
+            RECIPIENTEMAIL VARCHAR(255) NOT NULL
+        )"""
+    )
     try:
         config = load_config()
         with psycopg2.connect(**config) as conn:
